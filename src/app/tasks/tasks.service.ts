5f826e60f3b6be73b10dc04d2fdd7cecf753fdd6
khaled mohammed <dev.khaled.mohamed@gmail.com>
2024-10-18 23:00:49 +0300

    first commit

---
+++ src/app/tasks/tasks.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, signal } from '@angular/core';
+import { Task } from './task.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TasksService {
+tasks = signal<Task[]>([])
+
+addTask(taskData: {titls: string; description: string}) {
+const newTask: Task = {
+  ...taskData,
+  id: Math.random().toString(),
+  status: 'OPEN',
+  title: ''
+};
+  
+  this.tasks.update((oldTasks) => [...oldTasks, newTask])
+}
+
+
+}
